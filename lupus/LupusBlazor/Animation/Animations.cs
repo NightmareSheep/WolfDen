a9bf0cf655fe5234d534227c7f454957fc784877
Frank Nijmeijer <f.nijmeijer@students.uu.nl>
2021-12-23 14:33:59 +0100

    Updated UI

---
+++ lupus/LupusBlazor/Animation/Animations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LupusBlazor.Animation
+{
+    public enum Animations
+    {
+        none,
+        AttackUp,
+        AttackDown,
+        AttackLeft,
+        AttackRight,        
+        MoveUp,
+        MoveDown,
+        MoveLeft,
+        MoveRight,
+        DamagedFromUp,
+        DamagedFromDown,
+        DamagedFromLeft,
+        DamagedFromRight,
+        Cheer
+    }
+}
